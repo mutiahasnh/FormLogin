3f6178842ba812b3f8cef00969751a0067187ffb
Mutiahasnh <mutiahasnh@gmail.com>
2021-09-16 13:24:02 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugas8/AlertDialogManager.java
@@ -0,0 +1,22 @@
+package com.example.tugas8;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+public class AlertDialogManager {
+
+    public void showAlertDialog(Context context, String title, String message, Boolean status) {
+
+        AlertDialog alertDialog = new AlertDialog.Builder(context).create();
+        alertDialog.setTitle(title);
+        alertDialog.setMessage(message);
+
+        if (status != null)
+            alertDialog.setIcon((status) ? R.drawable.succes : R.drawable.fail);
+        alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+        alertDialog.show();
+    }}
